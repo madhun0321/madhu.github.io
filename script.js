d901d88a334332d90b22cd03f1606c7c8a858645
madhun0321 <130174686+madhun0321@users.noreply.github.com>
2023-06-05 22:45:32 +0530

    changed

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Welcome to the Page")
+var data1={image : "images.jpg",name : "Brahmanandam",designation : "Software Engineer"};
+var data2={image : "photo 4.jpeg",name : "Brahmi",designation : "Software Developer"};
+var isdata1 =true;
+var displaydata;
+var changingpic=function(){
+   if(isdata1)
+    {
+        displaydata=data2;
+        isdata1=false;
+    }
+    else{
+        displaydata=data1;
+        isdata1=true;
+    }
+    document.getElementById("myimage").src=displaydata.image;
+    document.getElementById("title").innerHTML=displaydata.name;
+    document.getElementById("mydesignation").innerHTML=displaydata.designation;
+    
+
+}
